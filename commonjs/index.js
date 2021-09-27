cd60bfc6b869c987a107f27ba910dcbb77ee9785
Angel Meraz <angel.meraz@itech.media>
2021-09-27 10:53:03 +0100

    updates CJS script with latest version

--- commonjs/index.js
+++ commonjs/index.js
@@ -1,4 +1,36 @@
+/**
+ * Replaces collection key values with matching overrides.
+ * Overrides structure must correspond to collection targetted key structures.
+ * @param {Object} classgroup 
+ * @param {Object} overrides 
+ * @returns {Object}
+ */
+ function setOverrides(collection = {}, overrides = {}) {
+    if (!overrides || !Object.keys(overrides).length) return collection;
+
+    for (const key in overrides) {
+        if (Object.hasOwnProperty.call(collection, key)) {
+            if (Object.prototype.toString.call(overrides[key]) === '[object Object]') {
+                setOverrides(collection[key], overrides[key]);
+                continue;
+            }
+        }
+        
+        collection[key] = overrides[key];
+    }
+
+    return collection;
+}
+
+/**
+ * Flattens the collection parameter into a single dimension object where each key value is a string.
+ * If the overrides parameter is present it will replace the targetted key values before flattening.
+ * @param {Object} collection 
+ * @param {Object} overrides 
+ * @returns 
+ */
 module.exports = function ClassGroup(collection = {}) {
+    collection = setOverrides(collection, overrides);
     const classGroup = {};
 
     function flatten(col, arr) {

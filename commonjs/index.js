bfe6e6c4b608add80fe0ed7de9aa42e6bb388aef
Angel Meraz <2284513+angelmeraz@users.noreply.github.com>
2023-11-27 16:03:32 +0000

    fix: CommonJS Typescript compilation; upgrades semantic-release

--- commonjs/index.js
+++ commonjs/index.js
@@ -1,3 +1,5 @@
+"use strict";
+exports.__esModule = true;
 /**
  * Performs type check of a string
  */
@@ -20,7 +22,7 @@ function overrideValue(collection, overrides) {
     if (instanceOfString(overrides) || instanceOfArray(overrides) || typeof overrides === 'boolean') {
         return overrides;
     }
-    for (const key in overrides) {
+    for (var key in overrides) {
         if (Object.prototype.hasOwnProperty.call(overrides, key)) {
             if (collection[key]) {
                 collection[key] = overrideValue(collection[key], overrides[key]);
@@ -36,7 +38,7 @@ function overrideValue(collection, overrides) {
  * Overrides parameter structure must correspond to that of the collection parameter targetted key structures.
  */
 function setOverrides(collection, overrides) {
-    for (const key in overrides) {
+    for (var key in overrides) {
         if (Object.prototype.hasOwnProperty.call(overrides, key)) {
             if (collection[key]) {
                 collection[key] = overrideValue(collection[key], overrides[key]);
@@ -51,18 +53,24 @@ function setOverrides(collection, overrides) {
  * Recursively flattens the collection parameter into a single dimension object transforming each key value into a string.
  * If the overrides parameter is present it will compare and replace the collection targetted key values before flattening.
  */
-module.exports = function ClassGroup(collection = {}, ...overrides) {
-    if (overrides === null || overrides === void 0 ? void 0 : overrides.length) {
-        overrides === null || overrides === void 0 ? void 0 : overrides.forEach((override) => (collection = setOverrides(collection, override)));
+module.exports = function ClassGroup(collection) {
+    if (collection === void 0) { collection = {}; }
+    var overrides = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        overrides[_i - 1] = arguments[_i];
     }
-    const classGroup = {};
+    if (overrides === null || overrides === void 0 ? void 0 : overrides.length) {
+        overrides === null || overrides === void 0 ? void 0 : overrides.forEach(function (override) { return (collection = setOverrides(collection, override)); });
+    }
+    var classGroup = {};
     function flatten(col, arr) {
         if (instanceOfString(col)) {
             arr.push(col);
             return arr;
         }
         if (instanceOfArray(col)) {
-            for (const value of col) {
+            for (var _i = 0, col_1 = col; _i < col_1.length; _i++) {
+                var value = col_1[_i];
                 arr.push(value);
             }
             return arr;
@@ -70,14 +78,14 @@ module.exports = function ClassGroup(collection = {}, ...overrides) {
         if (typeof col === 'boolean') {
             return arr;
         }
-        for (const key in col) {
+        for (var key in col) {
             if (Object.prototype.hasOwnProperty.call(col, key)) {
                 flatten(col[key], arr);
             }
         }
         return arr;
     }
-    for (const key in collection) {
+    for (var key in collection) {
         if (Object.prototype.hasOwnProperty.call(collection, key)) {
             classGroup[key] = flatten(collection[key], []).filter(Boolean).join(' ');
         }
